71702fbb6d5cb74a38166d8cde36d339d34c3773
Richter96 <castiglione96.rc@gmail.com>
2023-03-22 15:27:18 +0100

    create a markup for team

--- asset/js/main.js
+++ asset/js/main.js
@@ -38,43 +38,56 @@ Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 //creiamo un array di oggetti con all'interno i membri del team
 const teamMembers = [
     {
-        nome:'waynWayne Barnette',
-        ruolo:'Founder & CEO',
-        foto:'wayne-barnett-founder-ceo.jpg'
+        nome: 'WaynWayne Barnette',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
     },
     {
-        nome:'Angela Caroll',
-        ruolo:'Chief Editor',
-        foto:'angela-caroll-chief-editor.jpg'
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'
     },
     {
-        nome:'Walter Gordon',
-        ruolo:'Office Manager',
-        foto:'walter-gordon-office-manager.jpg'
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg'
     },
     {
-        nome:'Angela Lopez',
-        ruolo:'Social Media Manage',
-        foto:'angela-lopez-social-media-manager.jpg'
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manage',
+        foto: 'angela-lopez-social-media-manager.jpg'
     },
     {
-        nome:'Scott Estrada',
-        ruolo:'Developer',
-        foto:'scott-estrada-developer.jpg'
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg'
     },
     {
-        nome:'Barbara Ramos',
-        ruolo:'Graphic Designer',
-        foto:'barbara-ramos-graphic-designer.jpg'
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg'
     },
-    
+
 ]
+
+//richiao elemento del dom nel quale inserire il markup
+const rowEl = document.querySelector('.row')
 
 for (let i = 0; i < teamMembers.length; i++) {
     const Thisteam = teamMembers[i];
     console.log('nome', Thisteam.nome);
     console.log('ruolo', Thisteam.ruolo);
     console.log('foto', Thisteam.foto);
-
-    
+    const markupTeam = `
+    <div class="col">
+        <div class="card">
+            <img src="./asset/img/${Thisteam.foto}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${Thisteam.nome}</h5>
+                    <p class="card-text">${Thisteam.ruolo}</p>
+                </div>
+        </div>
+    </div>
+    `
+    rowEl.insertAdjacentHTML('beforeend', markupTeam)
 }
